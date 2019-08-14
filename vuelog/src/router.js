695a3982bdcf33d76c3d3b089239166b68673387
zl64183810aaa <52234178+zl64183810aaa@users.noreply.github.com>
2019-08-14 20:26:10 +0800

    8/14

--- vuelog/src/router.js
+++ vuelog/src/router.js
@@ -1,23 +1,18 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Index from './views/index.vue'
+import Login from './views/login.vue'
+import Reg from './views/reg.vue'
+import Myself from './views/myself.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
+    { path: '/',component: Index},
+    {path:'/login',component:Login},
+    {path:'/reg',component:Reg},
+    {path:'/myself',component:Myself},
+    
   ]
 })
