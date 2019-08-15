39959a6f5ef2a9d4df39c82052898cf4323ffbb2
zl64183810aaa <52234178+zl64183810aaa@users.noreply.github.com>
2019-08-15 20:01:52 +0800

    8/15_2

--- vuelog/src/main.js
+++ vuelog/src/main.js
@@ -3,9 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import MintUI from "mint-ui"
+import axios from "axios"
 
+axios.defaults.withCredentials=true
+Vue.prototype.axios=axios;
 Vue.use(MintUI)
 Vue.config.productionTip = false
+
+
 
 new Vue({
   router,
